734e2afb6029516cf4f978ca946b17e3530f09a3
sanjuhassan79 <sanjuhassan916@gmail.com>
2021-10-01 23:43:04 +0600

    shop cart updatet

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -36,7 +36,22 @@ const Shop = () => {
     }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        const exists=cart.find(pb=>pb.key===product.key)
+let newCart=[]
+if(exists){
+const rest=cart.filter(pb=>pb.key !== product.key)
+exists.quantity=exists.quantity+1;
+newCart = [...rest, product]
+}
+else{
+
+    product.quantity=1
+    newCart = [...cart, product]
+}
+
+
+
+      
         setCart(newCart);
         // save to local storage (for now)
         addToDb(product.key);
