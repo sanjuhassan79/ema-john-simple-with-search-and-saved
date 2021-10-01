3129c056102ae0c2b07b09f12694b26cc83cc32b
sanjuhassan79 <sanjuhassan916@gmail.com>
2021-10-01 14:25:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
-
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 function App() {
   return (
     <div>
+     
+
+      <Router>
+      <div>
       <Header></Header>
-      <Shop></Shop>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+          <Shop></Shop>
+          </Route>
+          <Route path="/Shop">
+          <Shop></Shop>
+          </Route>
+          <Route path="/OrderReview">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/Inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+
+
+     
     </div>
   );
 }
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Reviewitem from '../Reviewitem/Reviewitem';
 
 const OrderReview = () => {
+
+    const [products]=useProducts()
+    const [cart,setCart]=useCart(products )
+
+const handleRemove=key=>{
+const newCart=cart.filter(product=>product.key !==key)
+setCart(newCart)
+
+removeFromDb(key)
+}
+
+
+
+
+
     return (
         <div>
-            <h2>This is Order Review</h2>
+           <div className="shop-container">
+                <div className="product-container">
+               { cart.map(product => <Reviewitem
+                            key={product.key}
+                            product={product}
+                            handleRemove={handleRemove}
+                        >
+                        </Reviewitem>)
+                    }
+                        
+                </div>
+                <div className="cart-container">
+                    <Cart cart={cart}></Cart>
+                </div>
+            </div>
         </div>
     );
 };
